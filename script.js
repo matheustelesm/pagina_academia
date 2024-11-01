f77431cbcf84e4cff299317c20220428bb11f4e5
Matheus Teles <161986043+matheustelesm@users.noreply.github.com>
2024-11-01 10:39:31 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('a');
+
+buttons.forEach(button => {
+        button.addEventListener('mouseover', () => {
+        button.style.cursor = 'pointer';
+    });
+});
+const button = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+        button.addEventListener('mouseover', () => {
+        button.style.cursor = 'pointer';
+    });
+});
+
